fc2234852ca64748bc64c5ff81d40a8dae6fcde5
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-07 17:03:55 +0530

    Express Throw error

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -12,6 +12,13 @@ const getContacts = (req,res)  => {
 //@access public
 
 const createContact =(req,res)  => {
+    console.log("The request body is:",req.body);
+    const {name,email,contact}=req.body;
+    if(!name || !email || !phone)
+    {
+        res.status(400);
+        throw new Error("All files are mandatory");
+    }
     res.status(200).json({message:"Create Contacts"});
 };
 
--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -3,14 +3,8 @@ const router = express.Router();
 const{getContacts ,createContact, getContact, updateContact, deleteContact} = require("../controllers/contactController");
 
 
-router.route("/").get(getContact);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/").post(createContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
