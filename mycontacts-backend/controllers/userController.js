8f2f2aca341cd8a3454b915b660a148fcead1d6a
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-08 15:08:52 +0530

    JWT access Token and user login

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
-const bycrpt = require("bcrypt");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 //@desc Register a user
 //@route POST /api/users/register
@@ -20,6 +21,23 @@ const registerUser = asyncHandler(async(req,res) =>{
     //Hashpassword
     const hashedPassword = await bcrypt.hash(password,10);
     console.log("Hashed password: ", hashedPassword);
+    const user = await User.create({
+       username,
+       email,
+       password: hashedPassword,
+    });
+
+    console.log(`User Created ${user}`);
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            email: user.email
+        });
+    }
+    else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({message: "Register the user"});
 });
  
@@ -28,8 +46,33 @@ const registerUser = asyncHandler(async(req,res) =>{
 //@access public
 
 const loginUser = asyncHandler(async(req,res) =>{
-    res.json({message: "Login user"});
+    const {email, password} = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error ("All files are mandatory");
+    }
+    const user = await User.findOne({email});
+
+    //compare password with the used hashpassword
+    if (user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+
+        },process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:"1m"}
+        );
+        res.status(200).json({accessToken});
+    }else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
 });
+
+
 
 //@desc current user info
 //@route POST /api/users/current
