41722521d6659ae4c3463f6671a99884609820bc
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-08 16:29:06 +0530

    Protecting Routes - Contact

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -6,7 +6,7 @@ const Contact = require("../models/contactModel");
 //@access private
 
 const getContacts = asyncHandler(async(req,res)  => {
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({user_id:req.user.id});
     res.status(200).json({contacts});
 });
 
--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const{getContacts ,createContact, getContact, updateContact, deleteContact} = require("../controllers/contactController");
+const{getContacts ,
+    createContact, 
+    getContact, 
+    updateContact,
+     deleteContact
+    } = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
 
+router.use(validateToken);
 
 router.route("/").get(getContacts).post(createContact);
 
