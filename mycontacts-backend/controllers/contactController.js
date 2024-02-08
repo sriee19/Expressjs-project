287f54fe46a253933cb1f7139d47ca0d6b321fcc
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-08 11:46:18 +0530

    CRUD Get contact

--- mycontacts-backend/controllers/contactController.js
+++ mycontacts-backend/controllers/contactController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler");
-
+const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
 const getContacts = asyncHandler(async(req,res)  => {
-    res.status(200).json({message:"Get all contacts"});
+    const contacts = await Contact.find();
+    res.status(200).json({contacts});
 });
 
 
@@ -16,13 +17,19 @@ const getContacts = asyncHandler(async(req,res)  => {
 
 const createContact =asyncHandler(async(req,res)  => {
     console.log("The request body is:",req.body);
-    const {name,email,contact}=req.body;
+    const {name,email,phone}=req.body;
     if(!name || !email || !phone)
     {
         res.status(400);
         throw new Error("All files are mandatory");
     }
-    res.status(201).json({message:"Create Contacts"});
+
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    });
+    res.status(201).json({contact});
 });
 
 
