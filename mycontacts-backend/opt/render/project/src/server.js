2728bc344cd620a13ff2446d02c041a7d77b70d4
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-12 16:49:28 +0530

    2

--- mycontacts-backend/opt/render/project/src/server.js
+++ mycontacts-backend/opt/render/project/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const errorHandler = require("./middleware/errorHandler");
-const connectDb = require("./config/dbConnection");
+const errorHandler = require("../../../../middleware/errorHandler");
+const connectDb = require("../../../../config/dbConnection");
 const dotenv = require("dotenv").config();
 const server = require('/opt/render/project/src/server.js');
 
@@ -11,8 +11,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/contacts", require("./routes/contactRoutes") );
-app.use("/api/user", require("./routes/userRoutes") );
+app.use("/api/contacts", require("../../../../routes/contactRoutes") );
+app.use("/api/user", require("../../../../routes/userRoutes") );
 app.use(errorHandler);
 
 app.listen(port, () => {
