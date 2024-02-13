d0e7b5512751310b6b83a0b29d58ecca2ce63d96
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-13 15:00:35 +0530

    port number changes

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async() =>{
     try{
-        console.log("CONNECTION_STRING:", process.env.CONNECTION_STRING);
+        // console.log("CONNECTION_STRING:", process.env.CONNECTION_STRING);
 
         const connect = await mongoose.connect(process.env.CONNECTION_STRING)
         console.log("Database connected:",
