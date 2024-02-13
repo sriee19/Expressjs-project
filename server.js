8cf9a2ee46a0423424a7ac54d6fc31802a2616e5
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-13 13:55:54 +0530

    port changed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv").config();
 connectDb();
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes") );
