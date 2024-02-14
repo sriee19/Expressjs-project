a5828a382ff42984f15e913f6a2413b1a4ae914d
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-14 14:44:05 +0530

    db connection time out extended

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,18 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDb = async() =>{
-    try{
-        // console.log("CONNECTION_STRING:", process.env.CONNECTION_STRING);
-
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
-        console.log("Database connected:",
-                    connect.connection.host,
-                    connect.connection.name
-                     );
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-    }
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, 
+    });
+    console.log("Database connected:", connect.connection.host, connect.connection.name);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDb;
