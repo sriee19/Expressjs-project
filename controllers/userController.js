11c969df7fc023738829a74f37f3eb8773f65648
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-19 11:16:28 +0530

    display users changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async(req,res) =>{
     if(userAvailable){
         res.status(400);
         throw new Error("User already registered");
-    }
+    };
 
     //Hashpassword
     const hashedPassword = await bcrypt.hash(password,10);
@@ -72,24 +72,24 @@ const loginUser = asyncHandler(async(req,res) =>{
     }
 });
 
-    //@desc Display all registered users' usernames
-    //@route GET /api/users/displayAll
-    //@access public
+//     //@desc Display all registered users' usernames
+//     //@route GET /api/users/displayAll
+//     //@access public
 
-    const displayUser = asyncHandler(async (req, res) => {
-        try {
-            const allUsers = await User.find();
-            if ( allUsers.length > 0) {
-                const usernames = allUsers.map(user => user.username);
-                res.status(200).json(usernames);
-            } else {
-                res.status(404).json({ message: "No users found" });
-            }
-        } catch (error) {
-            console.error("Error fetching users:", error);
-            res.status(500).json({ message: "Internal Server Error" });
-        }
-});
+//     const displayUser = asyncHandler(async (req, res) => {
+//         try {
+//             const allUsers = await User.find();
+//             if ( allUsers.length > 0) {
+//                 const usernames = allUsers.map(user => user.username);
+//                 res.status(200).json(usernames);
+//             } else {
+//                 res.status(404).json({ message: "No users found" });
+//             }
+//         } catch (error) {
+//             console.error("Error fetching users:", error);
+//             res.status(500).json({ message: "Internal Server Error" });
+//         }
+// });
     
     
     //@desc Display all registered users
