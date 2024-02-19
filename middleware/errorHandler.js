0d4c5a87b5694cea485515c8072a29176b8f225e
SrisanjanaA <srisanjanaarunkumar@gmail.com>
2024-02-19 11:09:00 +0530

    Error Handlers changes

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,47 +1,33 @@
-const {constants} = require ("../constants")
-const errorHandler = (err,req,res,next) => {
-    const statuscode = res.statuscode ? res.statuscode : 500;
-   switch (statuscode) {
-    case constants.VALIDATION_ERROR:
-        res.json({
-            title: "Validation Failed",
-             message: err.message,
-             stackTrace: err.stack,
-        })
-        break;
-        case constants.NOT_FOUND:
-            res.json({
-                title: "Not Found",
-                 message: err.message,
-                 stackTrace: err.stack,
-            })
-            break;
-            case constants.UNAUTHORIZED:
-                res.json({
-                    title: "Un authorized",
-                     message: err.message,
-                     stackTrace: err.stack,
-                })
-                break;
-            case constants.FORBIDDEN:
-                res.json({
-                    title: "Forbidden",
-                     message: err.message,
-                     stackTrace: err.stack,
-                })
-                break;
-            case constants.SERVER_ERROR:
-                res.json({
-                    title: "server error",
-                     message: err.message,
-                     stackTrace: err.stack,
-                })
-                break;
-    default:
-        console.log("No error, All good!")
-        break;
-   }
-   
-};
+const { constants } = require("../constants")
 
-module.exports = errorHandler;
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({ title: "Validation Error", message: err.message, stackTrace: err.stack })
+            break;
+
+        case constants.NOT_FOUND:
+            res.json({ title: "Not Found", message: err.message, stackTrace: err.stack })
+            break;
+
+        case constants.UNAUTHORIZED:
+            res.json({ title: "Unauthorized", message: err.message, stackTrace: err.stack })
+            break;
+
+        case constants.FORBIDDEN:
+            res.json({ title: "Forbidden", message: err.message, stackTrace: err.stack })
+            break;
+
+        case constants.SERVER_ERROR:
+            res.json({ title: "Server Error", message: err.message, stackTrace: err.stack })
+            break;
+
+        default:
+            console.log("All set to go");
+            break;
+    }
+}
+
+module.exports = errorHandler
--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -1,26 +1,32 @@
 const asyncHandler = require("express-async-handler");
-
 const jwt = require("jsonwebtoken");
 
-const validateToken = asyncHandler(async(req,res,next) =>{
- let token; 
- let authHeader = req.headers.Authorization || req.headers.authorization;
- if(authHeader && authHeader.startsWith("Bearer")){
-    token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-        if(err){
-            res.status(401);
-            throw new Error("User is not authorised");
-        }
-        req.user = decoded.user;
-        next();
-    });
+const validateToken = asyncHandler(async (req, res, next) => {
+    let token;
+    let authHeader = req.headers.authorization || req.headers.Authorization;
     
-    if(!token){
-        res.status(401);
-        throw new Error("User is not authorised ")
-    }
-}
-});
+    if (authHeader && authHeader.startsWith("Bearer")) {
+        token = authHeader.split(" ")[1]
+
+        jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+            if (err) {
+                res.status(401);
+                throw new Error("User is not authorized");
+            }
+            console.log(decoded.user);
+            req.user = decoded.user;
+            next();
+
+        });
+        
+        if (!token) {
+            res.status(401);
+            throw new Error("User is not authorized or token is missing")
+        }
+
+    } 
+
+   
+})
 
 module.exports = validateToken;
